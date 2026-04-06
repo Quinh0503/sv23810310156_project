1db26961cccf2f70aeeaea80c83ebb0cb9e4dc36
Quinh0503 <dangquynhhd6505@gmail.com>
2026-04-06 15:35:32 +0700

    update category

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Models\Category;
-use Filament\Forms;
+// use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
