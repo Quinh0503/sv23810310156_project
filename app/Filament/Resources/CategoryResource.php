873b0c0a92db53818a6f3153a8bceacf2a2bd9a0
Quinh0503 <dangquynhhd6505@gmail.com>
2026-04-06 15:36:45 +0700

    update category

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Models\Category;
-// use Filament\Forms;
+use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -15,7 +15,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static ?string $slug = 'sv23810310156-categories';
+    // protected static ?string $slug = 'sv23810310156-categories';
 
     protected static ?string $navigationIcon = 'heroicon-o-tag';
 
