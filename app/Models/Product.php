83401f585e22409ac6d26ee0195ce7d1969dafc3
Quinh0503 <dangquynhhd6505@gmail.com>
2026-04-06 15:31:19 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $table = 'sv23810310156_products';
+
+    protected $fillable = [
+        'category_id', 'name', 'slug', 'description', 
+        'price', 'stock_quantity', 'image_path', 'status', 'warranty_months'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
